1200cdda60945c34ec27c94cfd0376667df0292b
batmantis <batmantisex@gmail.com>
2016-06-03 14:30:43 -0400

    table created by directive

--- table/table.directive.js
+++ table/table.directive.js
@@ -4,22 +4,30 @@ game.directive('board', function () {
   return {
     restrict: 'E',
     template: function () {
+      var height = 10
+      var width = 10
       // create <table> element
-      // var goltable = $scope.document.createElement("tbody");
-      //
-      // // build Table HTML
-      // var tablehtml = '';
-      // for (var h=0; h<this.height; h++) {
-      //   tablehtml += "<tr id='row+" + h + "'>";
-      //   for (var w=0; w<this.width; w++) {
-      //     tablehtml += "<td data-status='dead' id='" + w + "-" + h + "'></td>";
-      //   }
-      //   tablehtml += "</tr>";
-      // }
-      // goltable.innerHTML = tablehtml;
-      //
-      // return goltable
-      return 'test'
+      var goltable = document.createElement("tbody");
+
+      // build Table HTML
+      var tablehtml = '';
+      for (var h=0; h<height; h++) {
+        tablehtml += "<tr id='row+" + h + "'>";
+        for (var w=0; w<width; w++) {
+          tablehtml += "<td data-status='dead' id='" + w + "-" + h + "'></td>";
+        }
+        tablehtml += "</tr>";
+      }
+
+      console.log(typeof tablehtml)
+
+      goltable.innerHTML = tablehtml;
+      // return '<h1>hello</h1>'
+      return '<table id="board"><tbody>' + tablehtml + '</tbody></table>'
+    },
+    link: function(scope, element, attr) {
+      scope.height = [0, 1, 2, 3]
+      scope.width = [0, 1, 2, 3]
     }
   }
 });
