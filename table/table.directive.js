b16e45670f73090467e57564fdb376ddd8ce7680
batmantis <batmantisex@gmail.com>
2016-06-03 14:50:37 -0400

    removed angular

--- table/table.directive.js
+++ table/table.directive.js
@@ -3,31 +3,36 @@
 game.directive('board', function () {
   return {
     restrict: 'E',
-    template: function () {
-      var height = 10
-      var width = 10
-      // create <table> element
-      var goltable = document.createElement("tbody");
-
-      // build Table HTML
-      var tablehtml = '';
-      for (var h=0; h<height; h++) {
-        tablehtml += "<tr id='row+" + h + "'>";
-        for (var w=0; w<width; w++) {
-          tablehtml += "<td data-status='dead' id='" + w + "-" + h + "'></td>";
-        }
-        tablehtml += "</tr>";
-      }
-
-      console.log(typeof tablehtml)
-
-      goltable.innerHTML = tablehtml;
-      // return '<h1>hello</h1>'
-      return '<table id="board"><tbody>' + tablehtml + '</tbody></table>'
+    scope: {
+      height: '@',
+      width: '@'
     },
+    // template: function () {
+    //   // var height = 10
+    //   // var width = 10
+    //
+    //   // build Table HTML
+    //   var tablehtml = '';
+    //   for (var h=0; h<height; h++) {
+    //     tablehtml += "<tr id='row+" + h + "'>";
+    //     for (var w=0; w<width; w++) {
+    //       tablehtml += "<td data-status='dead' id='" + w + "-" + h + "'></td>";
+    //     }
+    //     tablehtml += "</tr>";
+    //   }
+    //
+    //   return '<table id="board"><tbody>' + tablehtml + '</tbody></table>'
+    // },
+    template: `<table id="board">
+      <tbody>
+        <tr ng-repeat="el in new Array(+height) track by $index">
+          <td ng-repeat="el in width">
+          </td>
+        </tr>
+      </tbody>
+    </table>`,
     link: function(scope, element, attr) {
-      scope.height = [0, 1, 2, 3]
-      scope.width = [0, 1, 2, 3]
+
     }
   }
 });
